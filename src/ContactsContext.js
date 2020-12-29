3604623e4c99451d464aff6e12a7bbcb7050100b
TeflonTrout <jtkazo@gmail.com>
2020-12-28 19:00:07 -0500

    edited contacts state

--- src/ContactsContext.js
+++ src/ContactsContext.js
@@ -4,30 +4,30 @@ export const ContactsContext = createContext();
 
 export function ContactsProvider(props) {
     const [contacts, setContacts] = useState([
-        {
-            id: 1,
-            name: "Joe Smith",
-            phone: 1234567890,
-            email: "test@test.com",
-            address: [{
-                street: "555 Test Ln",
-                city: "testville",
-                state: "TS",
-                zip: 55555
-            }]
-        },
-        {
-            id: 2,
-            name: "Joe Smith",
-            phone: 1234567890,
-            email: "test@test.com",
-            address: [{
-                street: "555 Test Ln",
-                city: "testville",
-                state: "TS",
-                zip: 55555
-            }]
-        }
+        // {
+        //     id: 1,
+        //     name: "Joe Smith",
+        //     phone: 1234567890,
+        //     email: "test@test.com",
+        //     address: [{
+        //         street: "555 Test Ln",
+        //         city: "testville",
+        //         state: "TS",
+        //         zip: 55555
+        //     }]
+        // },
+        // {
+        //     id: 2,
+        //     name: "Joe Smith",
+        //     phone: 1234567890,
+        //     email: "test@test.com",
+        //     address: [{
+        //         street: "555 Test Ln",
+        //         city: "testville",
+        //         state: "TS",
+        //         zip: 55555
+        //     }]
+        // }
         ])
     return (
         <ContactsContext.Provider value={[contacts, setContacts]}>
